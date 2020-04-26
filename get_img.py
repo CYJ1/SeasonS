2cda02e238e5823cc49bc5a8d129dcb1527e8ebf
CYJ1 <yungie22287@gmail.com>
2020-04-26 23:16:11 +0900

    Crawling goolge_image
    구글 대표사진 크롤링 코드 작성중.. 수정중..!

--- get_img.py
+++ get_img.py
@@ -1,27 +1,97 @@
-import requests
+import argparse
+import json
+import itertools
+import logging
+import re
+import os
+import uuid
+import sys
+from urllib.request import urlopen, Request
+
 from bs4 import BeautifulSoup
 
-# 타겟 URL을 읽어서 HTML를 받아오고,
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=headers)
+def configure_logging():
+    logger = logging.getLogger('chardet.charsetprober')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter('[%(asctime)s %(levelname)s %(module)s]: %(message)s'))
+    logger.addHandler(handler)
+    return logger
 
-# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
-soup = BeautifulSoup(data.text, 'html.parser')
-# select를 이용해서, tr들을 불러오기
-songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
-# body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+logger = configure_logging()
 
-# movies (tr들) 의 반복문을 돌리기
-rank = 1
-for song in songs:
-    # movie 안에 a 가 있으면,
-    title = song.select_one('td.info > a.title.ellipsis')
-    artist = song.select_one('td.info > a.artist.ellipsis')
-    if title is not None:
-        # a의 text를 찍어본다.
-        print(rank, '위', title.text.strip(), '/', artist.text.strip())
-        rank = rank + 1
+REQUEST_HEADER = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+
+
+def get_soup(url, header):
+    response = urlopen(Request(url, headers=header))
+    return BeautifulSoup(response, 'html.parser')
+
+
+def get_query_url(query):
+    return "https://www.google.co.in/search?q=%s&source=lnms&tbm=isch" % query
+
+
+def extract_images_from_soup(soup):
+    image_elements = soup.find_all("div", {"class": "rg_meta"})
+    metadata_dicts = (json.loads(e.text) for e in image_elements)
+    link_type_records = ((d["ou"], d["ity"]) for d in metadata_dicts)
+    return link_type_records
+
+
+def extract_images(query, num_images):
+    url = get_query_url(query)
+    logger.info("Souping")
+    soup = get_soup(url, REQUEST_HEADER)
+    logger.info("Extracting image urls")
+    link_type_records = extract_images_from_soup(soup)
+    return itertools.islice(link_type_records, num_images)
+
+
+def get_raw_image(url):
+    req = Request(url, headers=REQUEST_HEADER)
+    resp = urlopen(req)
+    return resp.read()
+
+
+def save_image(raw_image, image_type, save_directory):
+    extension = image_type if image_type else 'jpg'
+    file_name = uuid.uuid4().hex
+    save_path = os.path.join(save_directory, file_name)
+    with open(save_path, 'wb') as image_file:
+        image_file.write(raw_image)
+
+
+def download_images_to_dir(images, save_directory, num_images):
+    for i, (url, image_type) in enumerate(images):
+        try:
+            logger.info("Making request (%d/%d): %s", i, num_images, url)
+            raw_image = get_raw_image(url)
+            save_image(raw_image, image_type, save_directory)
+        except Exception as e:
+            logger.exception(e)
+
+
+def run(query, save_directory, num_images=10):
+    query = '+'.join(query.split())
+    logger.info("Extracting image links")
+    images = extract_images(query, num_images)
+    logger.info("Downloading images")
+    download_images_to_dir(images, save_directory, num_images)
+    logger.info("Finished")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Scrape Google images')
+    parser.add_argument('-s', '--search', default='bananas', type=str, help='search term')
+    parser.add_argument('-n', '--num_images', default=1, type=int, help='num images to save')
+    parser.add_argument('-d', '--directory', default='D:\SeasonS\SeasonS\img', type=str, help='save directory')
+    args = parser.parse_args()
+    run(args.search, args.directory, args.num_images)
+
+
+if __name__ == '__main__':
+    main()
